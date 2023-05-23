654fafeebef51865bb33c607ceab6ab9c577d611
Cesar Orozco <caesar.orz@gmail.com>
2023-05-23 10:35:49 -0600

    Change port for prod and db

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -132,10 +132,11 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'ecommerce_db',
+        'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
+        'PORT': 5432,
     }
 }
 
