86559b6988e6d258a2b7e09ab3629ba56c3e41a2
Cesar Orozco <caesar.orz@gmail.com>
2023-05-29 15:27:55 -0600

    Clean user serializers

--- ecommerce/user/serializers.py
+++ ecommerce/user/serializers.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField(read_only=True)
-    # _id = serializers.SerializerMethodField(read_only=True)
     isAdmin = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
