1fa702c7934adcbd9b229eb2cd5d51eff10ccc81
Cesar Orozco <caesar.orz@gmail.com>
2024-03-18 13:27:41 -0600

    Update dockerfile backend

--- frontend_ecommerce/src/screens/ProfileScreen.js
+++ frontend_ecommerce/src/screens/ProfileScreen.js
@@ -153,7 +153,6 @@ export default function ProfileScreen() {
                                         </tr>
                                 ))}
                             </tbody>
-
                         </Table>
                     )
                 }
