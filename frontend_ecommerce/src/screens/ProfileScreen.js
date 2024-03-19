5cf3d2bfce2959bf1b81ab7c9fe9f8eb540a1e83
Cesar Orozco <caesar.orz@gmail.com>
2024-03-19 09:31:29 -0600

    Fix Dockerfile for ecommerce app

--- frontend_ecommerce/src/screens/ProfileScreen.js
+++ frontend_ecommerce/src/screens/ProfileScreen.js
@@ -153,6 +153,7 @@ export default function ProfileScreen() {
                                         </tr>
                                 ))}
                             </tbody>
+
                         </Table>
                     )
                 }
